2fe2fc78e66c316839f67ef262abe975be31088a
garethrobertlee <garethrobertlee@gmail.com>
2018-01-29 14:22:24 +0000

    indent

--- src/BlockItem.js
+++ src/BlockItem.js
@@ -6,9 +6,9 @@ const BlockItem = (props) =>  {
 
     return (
       <div className={styles.item}>
-      <h3 style={{'fontFamily':props.fonts.headline}}>{props.product.headline}</h3>
-       <hr style={{'borderColor':props.colours.fg}}/>
-       <p style={{'fontFamily':props.fonts.text}}>{props.product.text}</p>
+        <h3 style={{'fontFamily':props.fonts.headline}}>{props.product.headline}</h3>
+        <hr style={{'borderColor':props.colours.fg}}/>
+        <p style={{'fontFamily':props.fonts.text}}>{props.product.text}</p>
      </div>
     )
   
